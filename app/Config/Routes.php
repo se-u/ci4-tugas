cdc95f35c6875e814cc093f54831ba147cbe5d10
Azriel Pamungkas <github.rwcoy@simplelogin.co>
2025-06-22 19:08:21 +0700

    menambahkan api data transaksi pembelian

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -47,3 +47,5 @@ $routes->get('get-cost', 'TransaksiController::getCost', ['filter' => 'auth']);
 $routes->get('faq', 'Home::faq', ['filter' => 'auth']);
 $routes->get('profile', 'Home::profile', ['filter' => 'auth']);
 $routes->get('contact', 'Home::contact', ['filter' => 'auth']);
+
+$routes->resource('api', ['controller' => 'apiController']);
